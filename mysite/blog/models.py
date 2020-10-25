c9fad48b9f8029afda0b6238e36da7109a52e7e7
ivoshet <ivoshet.world@gmail.com>
2020-10-25 21:48:39 +0500

    added html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# the query set to need for get objects collection from database
+# and for to work with its: filter, sorting
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -19,9 +27,15 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day,
+                                                 self.slug])
