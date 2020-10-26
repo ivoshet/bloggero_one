5a1f0118d0f1db5ad41ee427355eb802704c435e
ivoshet <ivoshet.world@gmail.com>
2020-10-26 18:35:42 +0500

    added email form widgets

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,13 @@
 # from mysite.blog.views import post_detail
+# from mysite.blog.views import PostListView
 from django.urls import path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail')
 ]
