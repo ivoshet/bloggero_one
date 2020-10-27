ca59322c2f4befe18d6115970c710c669efce2c5
ivoshet <ivan.v.shet@gmail.com>
2020-10-27 22:51:27 +0500

    added tags filter

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,8 +7,12 @@ from django.core.mail import send_mail
 from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 1)
     page = request.GET.get('page')
     try:
@@ -17,7 +21,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 # class PostListView(ListView):
