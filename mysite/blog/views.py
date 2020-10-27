fb1a30a30f1944f57f178e38a567815f20130c70
ivoshet <ivan.v.shet@gmail.com>
2020-10-27 22:25:04 +0500

    add function change to view list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,27 +4,27 @@ from django.core.paginator import Page, Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-# commit for github changes
+from taggit.models import Tag
 
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 1)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 1
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 1
+#     template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
